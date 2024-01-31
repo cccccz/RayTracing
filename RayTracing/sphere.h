08efd000554587335354e7faf81d9906878019ba
Zheng Chen <chenzheng0430@gmail.com>
2024-01-31 18:02:43 -0500

    bvh updated

--- RayTracing/sphere.h
+++ RayTracing/sphere.h
@@ -8,14 +8,23 @@ class sphere : public hittable {
 public:
     // Stationary Sphere
     sphere(point3 _center, double _radius, shared_ptr<material> _material)
-        : center1(_center), radius(_radius), mat(_material), is_moving(false) {}
+        : center1(_center), radius(_radius), mat(_material), is_moving(false)
+    {
+        auto rvec = vec3(radius, radius, radius);
+        bbox = aabb(center1 - rvec, center1 + rvec);
+    }
 
     // Moving Sphere
     sphere(point3 _center1, point3 _center2, double _radius, shared_ptr<material> _material)
         : center1(_center1), radius(_radius), mat(_material), is_moving(true)
     {
+        auto rvec = vec3(radius, radius, radius);
+        aabb box1(_center1 - rvec, _center1 + rvec);
+        aabb box2(_center1 - rvec, _center2 + rvec);
+        bbox = aabb(box1, box2);
         center_vec = _center2 - _center1;
     }
+    aabb bounding_box() const override { return bbox; }
 
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
@@ -52,6 +61,7 @@ private:
     shared_ptr<material> mat;
     bool is_moving;
     vec3 center_vec;
+    aabb bbox;
 
     point3 center(double time) const {
         // Linearly interpolate from center1  to center2 according to time, where t=0 yields
